b179c791a0b6afda6822f480c22f70b334a36c1a
LesVu <johndie005@gmail.com>
2022-07-24 12:39:23 +0000

    up

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 // puppeteer-extra is a wrapper around puppeteer,
 // it augments the installed puppeteer with plugin functionality
-const puppeteer = require("puppeteer-extra");
+const puppeteer = require('puppeteer-extra');
 
 // add stealth plugin and use defaults (all evasion techniques)
-const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
 // puppeteer usage as normal
-puppeteer.launch({ headless: true }).then(async (browser) => {
-  console.log("Check the bot tests..");
+puppeteer.launch({ headless: true }).then(async browser => {
+  console.log('Check the bot tests..');
   const page = await browser.newPage();
-  await page.goto("https://bot.sannysoft.com");
+  await page.goto('https://bot.sannysoft.com');
   await page.waitFor(5000);
-  await page.screenshot({ path: "bot-test-result.png", fullPage: true });
+  await page.screenshot({ path: 'bot-test-result.png', fullPage: true });
   await browser.close();
   console.log(`All done, check the bot result screenshot. `);
 });
