3fd1b8369d32de74713778e6f0c185f280318fe6
LesVu <johndie005@gmail.com>
2022-07-26 10:38:36 +0000

    Up

--- main.js
+++ main.js
@@ -6,18 +6,21 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 // puppeteer usage as normal
-puppeteer.launch({ headless: true }).then(async browser => {
-  console.log('Check the bot tests..');
-  // const page = await browser.newPage();
-  const page = (await browser.pages())[0];
-  await page.goto('');
-  await page.waitFor(5000);
-  await page.screenshot({ path: 'bot-test-result.png', fullPage: true });
-  await page.evaluate(() => {
-    let s = document.querySelectorAll(
-      '.container.index-container.index-popular div.gallery'
-    );
-    console.log(s);
+puppeteer
+  .launch({ headless: true, args: ['--no-sandbox'] })
+  .then(async browser => {
+    console.log('Check the bot tests..');
+    // const page = await browser.newPage();
+    const page = (await browser.pages())[0];
+    await page.goto('');
+    // await page.waitForSelector('.index-popular');
+    await page.waitForTimeout(30000);
+    await page.screenshot({ path: 'bot-test-result.png', fullPage: true });
+    await page.evaluate(() => {
+      let s = document.querySelectorAll(
+        '.container.index-container.index-popular div'
+      );
+      console.log(s);
+    });
+    await browser.close();
   });
-  await browser.close();
-});
